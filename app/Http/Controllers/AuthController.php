ca97396610a175fd8edf992bc17fe774cbff9df6
cristobaltt7 <cctt2004@gmail.com>
2025-06-13 11:34:26 +0200

    arreglo final login. Proyecto finalizado

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return redirect()->intended('/');
+            return redirect('/');
         }
 
         return back()->with('error', 'Credenciales incorrectas.');
